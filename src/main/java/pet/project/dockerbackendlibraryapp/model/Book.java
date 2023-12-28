4006d0ea5ecdac356b05cb74532795e870bef6ae
Уранов Роман <roman.uranov@gazprombank.ru>
2023-12-28 17:46:07 +0300

    Add Book

---
+++ src/main/java/pet/project/dockerbackendlibraryapp/model/Book.java
@@ -0,0 +1,17 @@
+package pet.project.dockerbackendlibraryapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+    private UUID id;
+    private String title;
+    private String author;
+    private Integer year;
+}
