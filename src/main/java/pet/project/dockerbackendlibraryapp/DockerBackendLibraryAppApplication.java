1a1cc70a62b77ebbddb1b127e1ec41ea101bd14b
Уранов Роман <roman.uranov@gazprombank.ru>
2023-12-28 11:03:46 +0300

    initial commit

---
+++ src/main/java/pet/project/dockerbackendlibraryapp/DockerBackendLibraryAppApplication.java
@@ -0,0 +1,13 @@
+package pet.project.dockerbackendlibraryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerBackendLibraryAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerBackendLibraryAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pet/project/dockerbackendlibraryapp/DockerBackendLibraryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pet.project.dockerbackendlibraryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerBackendLibraryAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
