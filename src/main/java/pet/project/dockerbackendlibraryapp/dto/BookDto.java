cf02a1577df6ce08c4d8a993c31db725685c435c
Уранов Роман <roman.uranov@gazprombank.ru>
2023-12-28 17:46:15 +0300

    Add BookDto

---
+++ src/main/java/pet/project/dockerbackendlibraryapp/dto/BookDto.java
@@ -0,0 +1,17 @@
+package pet.project.dockerbackendlibraryapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookDto {
+    private UUID id;
+    private String title;
+    private Integer year;
+    private String author;
+}
