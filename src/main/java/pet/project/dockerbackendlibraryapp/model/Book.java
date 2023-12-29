0240e04203dc6a61492d9b7b371bb13e18469f56
Уранов Роман <roman.uranov@gazprombank.ru>
2023-12-29 11:32:19 +0300

    Add Book

--- src/main/java/pet/project/dockerbackendlibraryapp/model/Book.java
+++ src/main/java/pet/project/dockerbackendlibraryapp/model/Book.java
@@ -14,4 +14,10 @@ public class Book {
     private String title;
     private String author;
     private Integer year;
+
+    public Book(String title, String author, Integer year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
 }
